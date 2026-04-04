2ac51e4583b31f5e2efca2dbe92f74ab43beeb09
Aakansha Raut <rautakansha62@gmail.com>
2026-04-04 14:37:20 +0530

    Delete assignment7 directory

--- assignment7/Q3.py
+++
@@ -1,9 +0,0 @@
-n = 5
-for i in range(1, n+1):
-    for j in range(1, i + 1):
-        if(j == 1) or (i == 5) or (j == i):
-          print(j, end=' ')
-        else:
-          print(' ', end=' ')
-        
-    print()
--- assignment7/Q6.py
+++
@@ -1,7 +0,0 @@
-for i in range(1, 6):
-    for j in range(1, 6):
-        if i == 1 or j == 1 or (i + j == 6):
-            print(f'{i if j==1 else (j if i==1 else 5)}', end=' ')
-        else:
-            print(' ', end=' ')
-    print()
